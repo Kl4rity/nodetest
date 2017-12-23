f17590e3a3b5b379f89787cc1195d07ac131997c
Kl4rity <clemens@stift.online>
2017-12-23 15:42:28 +0100

    Initialized a server on localhost. Responds to request.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 var http = require('http');
+
+var port = 8888;
+
+http.createServer(onRequest).listen(port);
+console.log("Server has started on port" + port);
+
+function onRequest(request, response){
+  response.writeHead(200);
+  response.write('Hello Noders');
+  response.end();
+}
