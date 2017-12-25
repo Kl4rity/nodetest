5e069cf212434889bd7b63125d0a3ea62f6e7247
Kl4rity <clemens@stift.online>
2017-12-25 20:07:34 +0100

    Starting to use Express Apps.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var http = require('http');
 var url = require('url');
 var show = require('./show');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
 
 var port = process.env.PORT || 8080;
 
-http.createServer(onRequest).listen(port);
-console.log("Server has started on port" + port);
+app.listen(port, () => {
+  console.log("Server has started on port "+port);
+})
 
-function onRequest(request, response){
+app.get(null , (request, response) => {
   var sPathName = url.parse(request.url).pathname;
   show.showPage(response, sPathName);
-}
+})
+
+// function onRequest(request, response){
+//   var sPathName = url.parse(request.url).pathname;
+//   show.showPage(response, sPathName);
+// }
