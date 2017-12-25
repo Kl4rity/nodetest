e59a7d65f270b3dbc507db629baf67bbbd8c1a67
Kl4rity <clemens@stift.online>
2017-12-25 11:51:29 +0100

    New page and link addedddeded to main page.

--- content.js
+++ content.js
@@ -1,8 +1,9 @@
 var dContentMap = {
 
-  '/' : '<h1>Welcome to my Hello World.</h1>',
+  '/' : '<h1>Welcome to my Hello World.</h1><p><ul><li><a href="/projects-notebook">Notebook</a></li></ul></p>',
   '/author' : '<h1>Author</h1><p>Hi, my name is Kl4rity and this is my first node app. I hope I can continue working with this stuff!</p>',
   '/privacy' : '<h1>Privacy</h1><p>Since this app does nothing, there is nothing to be worried about.</p>',
-  '/login' : '<h1>Login</h1><p>I might actually put a login here once I feel like it.</p>'
+  '/login' : '<h1>Login</h1><p>I might actually put a login here once I feel like it.</p>',
+  '/projects-notebook' : '<h1>Notebook</h1><p>I will implement a project-list with tags as to which technologies will be used within the projects. Should store everything persistently in a DB. Might even get a logon?</p>'
 }
 exports.dContentMap = dContentMap;
